77f04f2bacf26c9831db13d7824b2c455b6e4c33
Eliotlechat <marykita.contact@gmail.com>
2020-10-05 20:49:50 +0200

    PremierScript added

---
+++ Assets/Scripts/PremierScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PremierScript : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public Renderer rend;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend.material.color = Color.blue;
+        rb.useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
